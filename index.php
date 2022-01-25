42669c27557f88b4742346d4b3ff2394b7ce5207
salvomel <salv.mel@gmail.com>
2022-01-25 15:26:34 +0100

    header

--- index.php
+++ index.php
@@ -5,10 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google Faq</title>
-    
+
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <header>
+        <div class="header-top">
+            <div class="logo-container">
+                <img class="logo" src="img/google-logo.png" alt="Google Logo">
+                <span>Privacy e termini</span>
+            </div>
+
+            <div class="user">
+                <a href="#" class="app"><i class="fas fa-th"></i></a>
+                <a href="#" class="account">S</a>
+            </div>
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href="#">Introduzione</a></li>
+                <li><a href="#">Norme sulla privacy</a></li>
+                <li><a href="#">Termini di servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li><a class="selected" href="#">Domande Frequenti</a></li>
+            </ul>
+        </nav>
+    </header>
+    
     
 </body>
 </html>
